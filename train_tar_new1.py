f7e84fc73a0cba489b38c52d92969f3d6b156855
glbreeze <glbreeze@gmail.com>
2023-03-27 23:31:59 +0800

    check_cluster_BN

--- train_tar_new1.py
+++ train_tar_new1.py
@@ -1,27 +1,28 @@
 # encoding:utf-8
 
-import argparse, os, random
-import os.path as osp
 import numpy as np
-import pandas as pd
+import os.path as osp
+from datetime import date
+import argparse, os, random
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import network, loss
 from torch.utils.data import DataLoader
+import network
 from data_list import ImageList
+from train_tar_util import obtain_ncc_label, bn_adapt
+from loss import compute_dist, compute_loss
 from dataset.data_transform import TransformSW
-from sklearn.metrics import confusion_matrix
-from utils import Entropy, op_copy, lr_scheduler, image_train, image_test, cal_acc, print_args, log, set_log_path, pad_string
+from utils import op_copy, lr_scheduler, image_train, image_test, cal_acc, print_args, log, set_log_path, pad_string
 
 
 def trim_str(s, l):
-    ret = s[:l] if len(s)>=l else s
+    ret = s[:l] if len(s) >= l else s
     return ret
 
 
-mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
 visda_classes = ['aeroplane', 'bicycle', 'bus', 'car', 'horse' 'knife', 'motorcycle', 'person', 'plant', 'skateboard', 'train', 'truck']
+mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
 
 
 def data_load(args):
@@ -34,20 +35,24 @@ def data_load(args):
     txt_test = open(args.test_dset_path).readlines()
 
     dsize = len(txt_src)
-    tr_size = int(0.9*dsize)
+    tr_size = int(0.9 * dsize)
     _, te_txt = torch.utils.data.random_split(txt_src, [tr_size, dsize - tr_size])
     tr_txt = txt_src
 
     dsets["source_tr"] = ImageList(tr_txt, transform=image_train(), root=os.path.dirname(args.s_dset_path))
-    dset_loaders["source_tr"] = DataLoader(dsets["source_tr"], batch_size=train_bs, shuffle=True, num_workers=args.worker, drop_last=False)
+    dset_loaders["source_tr"] = DataLoader(dsets["source_tr"], batch_size=train_bs, shuffle=True,
+                                           num_workers=args.worker, drop_last=False)
     dsets["source_te"] = ImageList(te_txt, transform=image_test(), root=os.path.dirname(args.s_dset_path))
-    dset_loaders["source_te"] = DataLoader(dsets["source_te"], batch_size=train_bs, shuffle=True, num_workers=args.worker, drop_last=False)
+    dset_loaders["source_te"] = DataLoader(dsets["source_te"], batch_size=train_bs, shuffle=True,
+                                           num_workers=args.worker, drop_last=False)
 
     data_trans = TransformSW(mean, std, aug_k=1) if args.data_trans == 'SW' else image_train()
     dsets["target"] = ImageList(txt_tar, transform=data_trans, root=os.path.dirname(args.t_dset_path), ret_idx=True)
-    dset_loaders["target"] = DataLoader(dsets["target"], batch_size=train_bs, shuffle=True, num_workers=args.worker, drop_last=False)
+    dset_loaders["target"] = DataLoader(dsets["target"], batch_size=train_bs, shuffle=True, num_workers=args.worker,
+                                        drop_last=False)
     dsets["test"] = ImageList(txt_test, transform=image_test(), root=os.path.dirname(args.test_dset_path), ret_idx=True)
-    dset_loaders["test"] = DataLoader(dsets["test"], batch_size=train_bs * 3, shuffle=False, num_workers=args.worker, drop_last=False)
+    dset_loaders["test"] = DataLoader(dsets["test"], batch_size=train_bs * 3, shuffle=False, num_workers=args.worker,
+                                      drop_last=False)
 
     return dset_loaders
 
@@ -72,53 +77,21 @@ def analysis_target(args):
 
     optimizer = optim.SGD(param_group)
     optimizer = op_copy(optimizer)
-
     optimizer_c = optim.SGD(param_group_c)
     optimizer_c = op_copy(optimizer_c)
 
-    # ============== building feature bank and score bank (no grad) 遍历整个epoch
-    netF.eval()
-    netB.eval()
-    netC.eval()
+    # performance of original model
+    acc, cls_acc = cal_acc(dset_loaders["target"], netF, netB, netC, flag=True)
+    log("Source model accuracy on target domain {} \n classwise accuracy {} \n".format(acc, [round(e, 3) for e in cls_acc]))
 
-    target_loader = dset_loaders["target"]
-    num_sample = len(target_loader.dataset)
-    start_test = True
+    pred, _ = obtain_ncc_label(dset_loaders["target"], netF, netB, netC, args, log)
 
-    with torch.no_grad():
-        for batch, data in enumerate(target_loader):
-            img, label, idx = data
+    log("Adapt Batch Norm parameters")
+    netF, netB = bn_adapt(netF, netB, dset_loaders["target"], runs=1000)
 
-            outputs = netC(netB(netF(img.cuda())))
+    pred, _ = obtain_ncc_label(dset_loaders["target"], netF, netB, netC, args, log)
 
-            if start_test:
-                all_output = outputs.float().cpu()
-                all_label = label.float()
-                start_test = False
-            else:
-                all_output = torch.cat((all_output, outputs.float().cpu()), 0)
-                all_label = torch.cat((all_label, label.float()), 0)
 
-        all_output = nn.Softmax(dim=1)(all_output)
-        all_ent = Entropy(all_output)
-        _, predict = torch.max(all_output, 1)
-        accuracy = torch.sum(torch.squeeze(predict).float() == all_label).item() / float(all_label.size()[0])
-        mean_ent = torch.mean(all_ent).cpu().data.item()
-
-        matrix = confusion_matrix(all_label, torch.squeeze(predict).float())
-        class_acc = matrix.diagonal() / matrix.sum(axis=1) * 100
-        class_acc = [str(np.round(i, 2)) for i in class_acc]
-
-        log('overall accuracy on the target domain {:.4f}\n'.format(accuracy))
-        log('classwise accuracy on the target domain is \n ')
-        visda_classes_trim = [trim_str(e, 10) for e in visda_classes]
-        log('|'.join(pad_string(visda_classes_trim, [12] * len(visda_classes))) + '\n')
-        log('|'.join(pad_string(class_acc, [12] * len(class_acc))) + '\n')
-        log('confusion matrix is \n {}'.format(matrix))
-
-        out_table = torch.cat((all_output, predict.reshape(-1, 1), all_label.reshape(-1, 1), all_ent.reshape(-1, 1)),
-                              1).data.numpy()
-        pd.DataFrame(out_table).to_csv(os.path.join(args.output_dir, 'output.csv'))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Neighbors')
@@ -128,24 +101,24 @@ if __name__ == "__main__":
     parser.add_argument('--max_epoch', type=int, default=15, help="max iterations")
     parser.add_argument('--interval', type=int, default=15)
     parser.add_argument('--batch_size', type=int, default=64, help="batch_size")
-    parser.add_argument('--worker', type=int, default=4, help="number of workers")
+    parser.add_argument('--worker', type=int, default=2, help="number of workers")
     parser.add_argument('--dset', type=str, default='visda-2017')
     parser.add_argument('--lr', type=float, default=1e-3, help="learning rate")
     parser.add_argument('--net', type=str, default='resnet101')
     parser.add_argument('--seed', type=int, default=2021, help="random seed")
 
     parser.add_argument('--bottleneck', type=int, default=256)
-    parser.add_argument('--K', type=int, default=5)
-    parser.add_argument('--KK', type=int, default=5)
-    parser.add_argument('--epsilon', type=float, default=1e-5)
     parser.add_argument('--layer', type=str, default="wn", choices=["linear", "wn"])
     parser.add_argument('--classifier', type=str, default="bn", choices=["ori", "bn"])
-    parser.add_argument('--output', type=str, default='weight/')
-    parser.add_argument('--exp_name', type=str, default='analysis')
-    parser.add_argument('--data_trans', type=str, )
-    parser.add_argument('--tag', type=str, default='selfplus')
-    parser.add_argument('--da', type=str, default='uda')
-    parser.add_argument('--issave', type=bool, default=True)
+    parser.add_argument('--T', type=float, default=0.5, help='Temperature for creating pseudo-label')
+    parser.add_argument('--loss_type', type=str, default='sce', help='Loss function for target domain adaptation')
+
+    parser.add_argument('--distance', type=str, default='cosine', choices=['cosine', 'euclidean'])
+    parser.add_argument('--threshold', type=int, default=10, help='threshold for filtering cluster centroid')
+
+    parser.add_argument('--output', type=str, default='result/')
+    parser.add_argument('--exp_name', type=str, default='Clust_BN')
+    parser.add_argument('--data_trans', type=str, default='W')
     args = parser.parse_args()
 
     if args.dset == 'office-home':
@@ -178,7 +151,7 @@ if __name__ == "__main__":
         args.name = names[args.s][0].upper() + names[args.t][0].upper()
 
         if not osp.exists(args.output_dir):
-            os.mkdir(args.output_dir)
+            os.makedirs(args.output_dir)
         set_log_path(args.output_dir)
         log('save log to path {}'.format(args.output_dir_src))
         log(print_args(args))
---
+++ train_tar_util.py
@@ -0,0 +1,86 @@
+
+import numpy as np
+import os.path as osp
+from datetime import date
+import argparse, os, random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from scipy.spatial.distance import cdist
+
+
+def obtain_ncc_label(loader, netF, netB, netC, args, log):
+    """ prediction from Nearest Centroid Classifier """
+    start_test = True
+    with torch.no_grad():
+        iter_test = iter(loader)
+        for _ in range(len(loader)):
+            data = iter_test.next()
+            input, label = data[0], data[1]
+            feat = netB(netF(input.cuda()))
+            output = netC(feat)
+            if start_test:
+                all_feat = feat.float().cpu()
+                all_output = output.float().cpu()
+                all_label = label.float()
+                start_test = False
+            else:
+                all_feat = torch.cat((all_feat, feat.float().cpu()), 0)
+                all_output = torch.cat((all_output, output.float().cpu()), 0)
+                all_label = torch.cat((all_label, label.float()), 0)
+
+    all_output = nn.Softmax(dim=1)(all_output)
+    #ent = torch.sum(-all_output * torch.log(all_output + args.epsilon), dim=1)
+    #unknown_weight = 1 - ent / np.log(args.class_num)
+    _, predict = torch.max(all_output, 1)
+
+    acc = torch.sum(torch.squeeze(predict).float() == all_label).item() / float(all_label.size()[0])
+    if args.distance == 'cosine':
+        all_feat = torch.cat((all_feat, torch.ones(all_feat.size(0), 1)), 1)
+        all_feat = (all_feat.t() / torch.norm(all_feat, p=2, dim=1)).t()
+
+    all_feat = all_feat.float().cpu().numpy()  # [B, 257]
+    aff = all_output.float().cpu().numpy()     # [B, 12]
+
+    for _ in range(2):
+        initc = aff.transpose().dot(all_feat)  # [12, B] [B, 257]
+        initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
+        cls_count = np.eye(all_output.size(1))[predict].sum(axis=0)
+        labelset = np.where(cls_count>args.threshold)[0]
+
+        dd = cdist(all_feat, initc[labelset], args.distance)
+        pred_label = dd.argmin(axis=1)
+        new_pred = labelset[pred_label]
+        pred_score = torch.zeros_like(all_output).float()
+        pred_score[:, labelset] = nn.Softmax(dim=1)(torch.tensor(dd)).float()
+
+    new_acc = np.sum(new_pred == all_label.float().numpy()) / len(all_feat)
+    log('Nearest Clustering Centroid Based Accuracy = {:.2f}% -> {:.2f}%'.format(acc * 100, new_acc * 100))
+
+    return new_pred.astype('int'), pred_score
+
+
+def bn_adapt(netF, netB, data_loader, runs=10):
+    netF.eval()
+    netB.eval()
+    n_batch = 0
+    mom_pre = 0.1
+    for input in enumerate(data_loader):
+        mom_new = (mom_pre * 0.95)
+        mom_pre = mom_new
+        n_batch += 1
+        if n_batch >= runs:
+            break
+
+        for m in netF.modules():
+            if isinstance(m, torch.nn.modules.batchnorm._BatchNorm):
+                m.train()
+                m.momentum = mom_new + 0.005
+        for m in netB.modules():
+            if isinstance(m, torch.nn.modules.batchnorm._BatchNorm):
+                m.train()
+                m.momentum = mom_new + 0.005
+
+        _ = netB(netF(input.cuda()))
+    return netF, netB
