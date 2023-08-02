1cd215cc82d5b7b7e24b19213c46fe77bb5872bc
glbreeze <glbreeze@gmail.com>
2023-08-02 17:15:17 +0800

    domain_src

---
+++ dataset/domain_data.py
@@ -0,0 +1,216 @@
+
+import numpy as np
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torchvision
+from PIL import Image
+from torch.utils.data import Dataset
+from dataset.data_list import ImageList
+from dataset.data_transform import GaussianBlur, TwoCropsTransform
+
+normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+
+def image_train(resize_size=256, crop_size=224):
+    return transforms.Compose(
+        [
+            transforms.Resize((resize_size, resize_size)),
+            transforms.RandomCrop(crop_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+
+def image_target(resize_size=256, crop_size=224):
+    return transforms.Compose(
+        [
+            transforms.Resize((resize_size, resize_size)),
+            transforms.RandomCrop(crop_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+
+def image_test(resize_size=256, crop_size=224):
+    return transforms.Compose(
+        [
+            transforms.Resize((resize_size, resize_size)),
+            transforms.CenterCrop(crop_size),
+            # transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+
+
+
+def get_moco_base_augmentation0(min_scale=None, max_scale=None):
+    if min_scale is None:
+        min_scale = 0.2
+    if max_scale is None:
+        max_scale = 1.0
+    return [
+        transforms.RandomResizedCrop(224, scale=(min_scale, 1.0)),
+        transforms.RandomApply(
+            [transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8  # not strengthened
+        ),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.RandomApply([GaussianBlur(radius_min=0.1, radius_max=2.0)], p=0.5),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]
+
+def get_moco_base_augmentation1(min_scale=None):
+    if min_scale is None:
+        min_scale = 0.5
+    return [
+        transforms.RandomResizedCrop(224, scale=(min_scale, 1.0)),
+        transforms.RandomApply(
+            [transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8  # not strengthened
+        ),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.RandomApply([GaussianBlur(radius_min=0.1, radius_max=2.0)], p=0.5),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]
+
+def moco_transform(min_scales=None):
+    if min_scales is None:
+        return TwoCropsTransform(transforms.Compose(get_moco_base_augmentation0()),
+                                 transforms.Compose(get_moco_base_augmentation1()))
+    else:
+        return TwoCropsTransform(transforms.Compose(get_moco_base_augmentation0(min_scales[0])),
+                                 transforms.Compose(get_moco_base_augmentation1(min_scales[1])))
+
+
+def mn_transform(min_scales=None):
+    if min_scales is None:
+        m_scale = 0.2
+    else:
+        m_scale = min_scales[0]
+
+    return TwoCropsTransform(
+        transforms.Compose(get_moco_base_augmentation0(m_scale)),
+        transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.CenterCrop(224),
+            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.RandomApply([GaussianBlur(radius_min=0.1, radius_max=2.0)], p=0.5),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    )
+
+
+def mw_transform(min_scales=None):
+    resize_size = 256
+    crop_size = 224
+
+    return TwoCropsTransform(
+        transforms.Compose([
+            transforms.Resize((resize_size, resize_size)),
+            transforms.RandomCrop(crop_size),
+            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.RandomApply([GaussianBlur(radius_min=0.1, radius_max=2.0)], p=0.5),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize]),
+        transforms.Compose([
+            transforms.Resize((resize_size, resize_size)),
+            transforms.CenterCrop(crop_size),
+            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            transforms.RandomApply([GaussianBlur(radius_min=0.1, radius_max=2.0)], p=0.5),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize])
+    )
+
+
+def get_AutoAug(args):
+    if args.data_trans == 'ai':
+        return TwoCropsTransform(transforms.AutoAugment(transforms.AutoAugmentPolicy.IMAGENET),
+                                 transforms.AutoAugment(transforms.AutoAugmentPolicy.IMAGENET))
+
+    elif args.data_trans == 'ac':
+        return TwoCropsTransform(transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
+                                 transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10))
+
+def get_RandAug(args):
+    if args.data_aug is not None:
+        num_ops, magnitude = args.data_aug  # list
+    else:
+        num_ops, magnitude = 2, 9
+    return TwoCropsTransform(transforms.RandAugment(num_ops, magnitude),
+                             transforms.RandAugment(num_ops, magnitude))
+
+
+def domain_load(args, ret_idx=False):
+
+    ss = args.dset.split("2")[0]
+    tt = args.dset.split("2")[1]
+
+    map_dict = {'r': 'real', 's': 'sketch', 'c': 'clipart', 'p': 'painting'}
+    s = map_dict[ss]
+    t = map_dict[tt]
+
+    s_tr, s_ts = "./dataset/data_list/domainnet-126/{}_list.txt".format(s), "./dataset/data_list/domainnet-126/{}_list.txt".format(s)
+
+    txt_src = open(s_tr).readlines()
+    dsize = len(txt_src)
+    """tv_size = int(1.0 * dsize)
+    print(dsize, tv_size, dsize - tv_size)
+    s_tr, s_ts = torch.utils.data.random_split(txt_src, [tv_size, dsize - tv_size])"""
+    s_tr = txt_src
+    s_ts = txt_src
+
+    t_tr, t_ts = "./dataset/data_list/domainnet-126/{}_list.txt".format(t), "./dataset/data_list/domainnet-126/{}_list.txt".format(t)
+    prep_dict = {}
+    prep_dict["source"] = image_train()
+    prep_dict["target"] = image_target()
+    prep_dict["test"] = image_test()
+    train_source = ImageList(s_tr, transform=prep_dict["source"], root='../dataset/domainNet', ret_idx=ret_idx)
+    test_source = ImageList(s_tr, transform=prep_dict["source"], root='../dataset/domainNet', ret_idx=ret_idx)
+    train_target = ImageList(open(t_tr).readlines(), transform=prep_dict["target"], root='../dataset/domainNet', ret_idx=ret_idx)
+    test_target = ImageList(open(t_ts).readlines(), transform=prep_dict["test"], root='../dataset/domainNet', ret_idx=ret_idx)
+
+    dset_loaders = {}
+    dset_loaders["source_tr"] = DataLoader(
+        train_source,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.worker,
+        drop_last=False,
+    )
+    dset_loaders["source_te"] = DataLoader(
+        test_source,
+        batch_size=args.batch_size * 2,  # 2
+        shuffle=True,
+        num_workers=args.worker,
+        drop_last=False,
+    )
+    dset_loaders["target"] = DataLoader(
+        train_target,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.worker,
+        drop_last=False,
+    )
+    dset_loaders["test"] = DataLoader(
+        test_target,
+        batch_size=args.batch_size * 3,  # 3
+        shuffle=False,
+        num_workers=args.worker,
+        drop_last=False,
+    )
+    return dset_loaders
---
+++ domain/domain_src.py
@@ -0,0 +1,122 @@
+import copy
+import random
+import argparse
+import os.path as osp
+import torch.optim as optim
+from utils import *
+from model import network
+from model.loss import CrossEntropyLabelSmooth
+from dataset.domain_data import domain_load
+
+
+def train_source(args, log):
+    dset_loaders = domain_load(args)
+
+    # === set base network
+    netF = network.ResBase(res_name=args.net).cuda()
+    netB = network.feat_bootleneck(type='bn', feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
+    netC = network.feat_classifier(type='wn', class_num=args.class_num, bottleneck_dim=args.bottleneck).cuda()
+
+    param_group = [{'params': netF.parameters(), 'lr': args.lr},
+                   {'params': netB.parameters(), 'lr': args.lr * 10},
+                   {'params': netC.parameters(), 'lr': args.lr * 10}]
+    optimizer = optim.SGD(param_group, momentum=0.9, weight_decay=5e-4, nesterov=True)
+
+    acc_init = 0.0
+    for epoch in range(args.max_epoch):
+        netF.train()
+        netB.train()
+        netC.train()
+        iter_source = iter(dset_loaders["source_tr"])
+        for batch_idx, (inputs_source, labels_source) in enumerate(iter_source):
+            if inputs_source.size(0) == 1:
+                continue
+            inputs_source, labels_source = inputs_source.cuda(), labels_source.cuda()
+
+            output = netB(netF(inputs_source))
+            output = netC(output)
+
+            loss = CrossEntropyLabelSmooth(num_classes=args.class_num, epsilon=args.smooth)(output, labels_source)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        netF.eval()
+        netC.eval()
+        acc_s, _ = cal_acc(dset_loaders['source_te'], netF, netB, netC)
+        acc_t, _ = cal_acc(dset_loaders['test'], netF, netB, netC)
+        log('Source: {}, Iter:{}/{}; Src Acc = {:.2f}%, Tar Acc = {:.2f}%'.format(args.dset, epoch + 1, args.max_epoch,
+                                                                                  acc_s * 100, acc_t * 100))
+
+        if acc_t >= acc_init:
+            acc_init = acc_t
+            best_netF = copy.deepcopy(netF.state_dict())
+            best_netB = copy.deepcopy(netB.state_dict())
+            best_netC = copy.deepcopy(netC.state_dict())
+    torch.save(best_netF, osp.join(args.output_dir, "source_F.pt"))
+    torch.save(best_netB, osp.join(args.output_dir, "source_B.pt"))
+    torch.save(best_netC, osp.join(args.output_dir, "source_C.pt"))
+
+
+def test_target(args, log):
+    dset_loaders = domain_load(args)
+    ## set base network
+    netF = network.ResBase(res_name=args.net).cuda()
+    netB = network.feat_bootleneck(type='bn', feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
+    netC = network.feat_classifier(type='wn', class_num=args.class_num, bottleneck_dim=args.bottleneck).cuda()
+
+    netF.load_state_dict(torch.load(args.output_dir + '/source_F.pt'))
+    netB.load_state_dict(torch.load(args.output_dir + '/source_B.pt'))
+    netC.load_state_dict(torch.load(args.output_dir + '/source_C.pt'))
+
+    netF.eval()
+    netB.eval()
+    netC.eval()
+
+    acc, _ = cal_acc(dset_loaders['test'], netF, netB, netC)
+    log('Task: {}, Accuracy = {:.2f}%'.format(args.dset, acc * 100))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Domain Adaptation on office-home dataset')
+    parser.add_argument('--gpu_id', type=str, nargs='?', default='0', help="device id to run")
+    parser.add_argument('--max_epoch', type=int, default=100, help="maximum epoch")
+    parser.add_argument('--batch_size', type=int, default=64, help="batch_size")
+    parser.add_argument('--lr', type=float, default=0.001, help="learning rate")
+    parser.add_argument('--seed', type=int, default=2021, help="random seed")
+
+    parser.add_argument('--worker', type=int, default=4, help="number of workers")
+    parser.add_argument('--dset', type=str, default='c2r')
+
+    parser.add_argument('--net', type=str, default='resnet50', help="resnet50, resnet101")
+    parser.add_argument('--bottleneck', type=int, default=256)
+    parser.add_argument('--classifier', type=str, default="bn", choices=["ori", "bn"])
+    parser.add_argument('--layer', type=str, default="wn", help='type-classification layer', choices=["linear", "wn"])
+    parser.add_argument('--class_num', type=int, default=126)
+    parser.add_argument('--smooth', type=float, default=0.1)
+    args = parser.parse_args()
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+    SEED = args.seed
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    np.random.seed(SEED)
+    random.seed(SEED)
+    torch.backends.cudnn.deterministic = True
+
+    task = ['c', 'r', 's', 'p']
+    task_s = args.dset[0]
+    task.remove(task_s)
+    task_all = [task_s + '2' + i for i in task]
+
+    args.output_dir = osp.join('result/domain/source', 'seed' + str(args.seed), task_s)
+    ensure_path(args.output_dir)
+    set_log_path(args.output_dir)
+    log('save log to path {}'.format(args.output_dir))
+    log(print_args(args))
+
+    if not osp.exists(osp.join(args.output_dir + '/source_F.pt')):
+        train_source(args, log)
+        for task in task_all:
+            args.dset = task
+            test_target(args, log)
